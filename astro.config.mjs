95cd4a51fe173bc6770eac04e37acf8e5a070a2c
Chirag Singhal <chirag@oriz.in>
2026-06-22 22:58:48 +0530

    feat(ci): add GH Pages mirror workflow (CF primary + GH backup)

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,8 @@
 // @ts-check
 import { shell } from '@chirag127/astro-shell/shell'
 
-export default shell({ site: 'https://print.oriz.in', includeMdx: false })
+export default shell({
+  site: 'https://print.oriz.in',
+  base: process.env.PUBLIC_BASE_PATH ?? '/',
+  includeMdx: false,
+})
